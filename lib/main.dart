cdc1801838bba9ae4275f39d2d2347c6368397de
tanvu1810 <tanvukt2003@gmail.com>
2024-09-29 22:41:47 +0700

    Layout StartScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main(List<String> args) {
   runApp(
@@ -10,6 +11,45 @@ void main(List<String> args) {
                 colors: [Colors.deepPurple, Colors.deepPurpleAccent],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 300,
+                  color: Colors.white,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Learn Flutter The Fun Way !',
+                  style: GoogleFonts.aBeeZee(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.arrow_right_alt),
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        40,
+                      ),
+                      side: const BorderSide(
+                        width: 2,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  label: const Text('Start Quiz'),
+                )
+              ],
+            ),
           ),
         ),
       ),
