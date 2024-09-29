7309f744c3c003c0e341256fa6f0349be5015fa7
tanvu1810 <tanvukt2003@gmail.com>
2024-09-29 14:47:15 +0700

    start_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.deepPurple, Colors.deepPurpleAccent],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight),
+          ),
+        ),
+      ),
+    ),
+  );
+}
